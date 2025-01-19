6181a9ab23325c0af66c7d27c3b6164dc8bb2af2
Artyom <artyomkn@gmail.com>
2025-01-19 16:49:35 +0500

    1234

--- lab1/Variables.py
+++ lab1/Variables.py
@@ -19,3 +19,55 @@ print(type(y))
 a = 4
 A = "Sally"
 #A will not overwrite a
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+myfunc()
+
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
