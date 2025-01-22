ccacbccc88f6a94285013c3e6557367921226760
Artyom <artyomkn@gmail.com>
2025-01-22 11:39:58 +0500

    12345

---
+++ lab2/Operators.py
@@ -0,0 +1,86 @@
+"""
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+
+"""
+Python Arithmetic Operators
+Operator	Name		
++	        Addition	     	
+-	        Subtraction	
+*	        Multiplication	
+/	        Division		
+%	        Modulus		
+**	        Exponentiation		
+//	        Floor division
+"""
+
+"""
+Python Assignment Operators
+Operator	Example	
+=	        x = 5		
++=	        x += 3		
+-=	        x -= 3		
+*=	        x *= 3		
+/=	        x /= 3		
+%=	        x %= 3		
+//=	        x //= 3		
+**=	        x **= 3		
+&=	        x &= 3		
+|=	        x |= 3		
+^=	        x ^= 3		
+>>=	        x >>= 3		
+<<=	        x <<= 3		
+:=	        print(x := 3)
+"""
+
+"""
+Python Comparison Operators
+Operator	Name	                   Example	
+==	        Equal	                   x == y	
+!=	        Not equal	               x != y	
+>	        Greater than	           x > y	
+<	        Less than	               x < y	
+>=	        Greater than or equal to   x >= y	
+<=	        Less than or equal to	   x <= y
+"""
+
+"""
+Python Logical Operators
+Operator	Description	
+and 	    Returns True if both statements are true		
+or	        Returns True if one of the statements is true		
+not	        Reverse the result, returns False if the result is true
+"""
+
+"""
+Python Identity Operators
+Operator	Description	
+is 	        Returns True if both variables are the same object		
+is not	    Returns True if both variables are not the same object
+"""
+
+"""
+Python Membership Operators
+Operator	Description	
+in 	        Returns True if a sequence with the specified value is present in the object		
+not in	    Returns True if a sequence with the specified value is not present in the object
+"""
+
+"""
+Python Bitwise Operators
+Operator	Name	              Description	
+& 	        AND	                  Sets each bit to 1 if both bits are 1		
+|	        OR	                  Sets each bit to 1 if one of two bits is 1		
+^	        XOR	                  Set each bit to 1 if only one of two bits is 1		
+~	        NOT	                  Inverts all the bits	
+<<	        Zero fill left shift  Shift left by pushing zeros in from the right and let the leftmost bits fall off		
+>>	        Signed right shift	  Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+"""
