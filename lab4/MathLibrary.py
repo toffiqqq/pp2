55e05d2b849dc7e0eeff423a86179265f27954b4
Artyom <artyomkn@gmail.com>
2025-02-12 11:31:33 +0500

    123

---
+++ lab4/MathLibrary.py
@@ -0,0 +1,28 @@
+import math 
+#1
+a = int(input("Input degree: "))
+radian = a * (math.pi / 180)
+print(f"Output radian: {radian}")
+
+#2
+height1 = int(input("Height:"))
+first = int(input("First value:"))
+second = int(input("Second value:"))
+
+area1 = (first + second)/ 2 * height1
+print(f"Expected output: {area1}")
+
+#3
+side = int(input("Number of sides:"))
+length1 = int(input("Length of sides:"))
+
+area2 = (side * length1 ** 2)/(4 * math.tan(math.pi / side))
+
+print(f"Area of polygon: {area2}")
+
+#4
+length2 = int(input("Length of base: "))
+height2 = int(input("Height of parallelogram: "))
+
+area3 = length2 * height2
+print(f"Area of parallelogram: {area3}")
